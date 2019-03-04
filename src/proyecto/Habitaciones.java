ff106246f26dba0548da72c8cb217a5a116dbe9d
Andres <andrescamposmacias@gmail.com>
2019-03-04 12:02:04 +0100

    Crear clase habitaciones

---
+++ src/proyecto/Habitaciones.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author andres
+ */
+public class Habitaciones extends Estancia {
+    
+    private Luz luz;
+    private Persiana persiana;
+    private Camara camara;
+    
+    
+}
