53fec10270f336f9b2c8471b4ad15c14576f64b2
Andres <andrescamposmacias@gmail.com>
2019-03-04 11:58:53 +0100

    crear la clase persiana

---
+++ src/proyecto/Persiana.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author andres
+ */
+public class Persiana {
+    
+    private boolean estado;
+    private int largo;//cm
+    private int ancho;//cm
+    
+}
