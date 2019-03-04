35fb3d581e7bfba015b1e9c90a7ceffb26c501b9
Andres <andrescamposmacias@gmail.com>
2019-03-04 12:05:14 +0100

    Crear clase camara y central

---
+++ src/proyecto/Central.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author andres
+ */
+public class Central {
+    
+    private Estancia garaje;
+    private Estancia salon;
+    private Estancia dormitorio;
+    private Usuario usuario;
+    private LocalDate fechaInstalacion;
+    private String identificador;
+    
+        
+    
+}
