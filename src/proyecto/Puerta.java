78faf68f75576e130f6d5f34d06903f8c0c38586
Andres <andrescamposmacias@gmail.com>
2019-03-04 11:58:31 +0100

    crear la clase puerta

---
+++ src/proyecto/Puerta.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author andres
+ */
+public class Puerta {
+    
+    private boolean estado;
+    private int retardo;// segundos
+    
+    //java crea un constructor por defecto
+    //si no implementamos ninguno
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getRetardo() {
+        return retardo;
+    }
+
+    public void setRetardo(int retardo) {
+        this.retardo = retardo;
+    }
+
+    @Override
+    public String toString() {
+        return "Puerta{" + "estado=" + estado + ", retardo=" + retardo + '}';
+    }
+    
+    
+    
+}
