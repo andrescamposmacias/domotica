4d4c6ef3b28aeae9e108572330c35c9b28974217
Andres <andrescamposmacias@gmail.com>
2019-03-05 09:24:15 +0100

    insertar el metodo menu de la clase vista y comprobar que funciona

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -13,12 +13,14 @@ public class Main {
     
     public static void main(String[] args) {
         
-        Garaje g1 = new Garaje();
-        System.out.println(g1);
+//        Garaje g1 = new Garaje();
+//        System.out.println(g1);
+//        
+//        Garaje g2 = new Garaje(2, new Puerta(), 50);
+//        System.out.println(g2);
         
-        Garaje g2 = new Garaje(2, new Puerta(), 50);
-        System.out.println(g2);
-        
+        Vista.menu();
+                
     }
     
 }
