58d94a74ea293dede5d22659672151aa6730f70b
Andres <andrescamposmacias@gmail.com>
2019-03-17 20:11:22 +0100

    Añadir el atributo usuario al constructor, quitar el static del atributo usuario y mejorar el metodo comprobacionUsuario()

--- src/proyecto/Central.java
+++ src/proyecto/Central.java
@@ -15,23 +15,52 @@ public class Central {
     private Estancia garaje;
     private Estancia salon;
     private Estancia dormitorio;
-    private static Usuario usuario = new Usuario ("andres", "diegoCallate");
+    private Usuario usuario;
     private LocalDate fechaInstalacion;
     private String identificador;
     
    public static boolean comprobacionUsuario(){
        
-       return usuario.equals(Vista.login());
+       Usuario aux = new Usuario ("Andres", "Diego");
+       
+       return aux.equals(Vista.login());
     
    } 
 
-    public Central(Estancia garaje, Estancia salon, Estancia dormitorio, LocalDate fechaInstalacion, String identificador) {
+    public Central(Estancia garaje, Estancia salon, Estancia dormitorio, Usuario usuario, LocalDate fechaInstalacion, String identificador) {
         this.garaje = garaje;
         this.salon = salon;
         this.dormitorio = dormitorio;
+        this.usuario = usuario;
         this.fechaInstalacion = fechaInstalacion;
         this.identificador = identificador;
     }
+
+    public Estancia getGaraje() {
+        return garaje;
+    }
+
+    public Estancia getSalon() {
+        return salon;
+    }
+
+    public Estancia getDormitorio() {
+        return dormitorio;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getFechaInstalacion() {
+        return fechaInstalacion;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    
    
    
          
