9d0bcbce73a00fd825363c69f46bbc3740ba8945
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:34:54 +0100

    añadir el constructor, getters y setters y el metodo toString

--- src/proyecto/Luz.java
+++ src/proyecto/Luz.java
@@ -13,5 +13,31 @@ public class Luz {
     
     private boolean estado;
     private double consumo;
+
+    public Luz(boolean estado, double consumo) {
+        this.estado = estado;
+        this.consumo = consumo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+
+    @Override
+    public String toString() {
+        return "Luz{" + "estado=" + estado + ", consumo=" + consumo + '}';
+    }
     
 }
