9805ca18bde02a4848c076e95544888cf73caafd
Diego <diegofernandez29400@gmail.com>
2019-03-19 21:15:52 +0100

    Terminar de completar switch y metodos vacios

--- src/proyecto/Central.java
+++ src/proyecto/Central.java
@@ -56,6 +56,81 @@ public class Central {
             case CONSULTAR_PUERTA_GARAJE:
                 consultarEstadoGaraje();
                 break;
+            case CONSULTAR_PERSIANA_SALON:
+                consultarEstadoPersianaSalon();
+                break;
+            case SUBIR_PERSIANA_SALON:
+                subirPersianaSalon();
+                break;
+            case BAJAR_PERSIANA_SALON:
+                bajarPersianaSalon();
+                break;
+            case MEDIAALTURA_PERSIANA_SALON:
+                mediaAlturaPersianaSalon();
+                break;
+            case ENCENDER_LUCES_SALON:
+                encenderLucesSalon();
+                break;
+            case APAGAR_LUCES_MANUAL_SALON:
+                apagarLucesManualSalon();
+                break;
+            case APAGAR_LUCES_GENERAL_SALON:
+                apagarLucesGeneralSalon();
+                break;
+            case APAGAR_LUCES_ECO_SALON:
+                apagarLucesEcoSalon();
+                break;
+            case CONSULTAR_LUCES_SALON:
+                consultarLucesSalon();
+                break;
+            case ENCENDER_VIGILANCIA_SALON:
+                encenderVigilanciaSalon();
+                break;
+            case APAGAR_VIGILANCIA_SALON:
+                apagarVigilanciaSalon();
+                break;
+            case CONSULTAR_VIGILANCIA_SALON:
+                consultarVigilanciaSalon();
+                break;
+            case CONSULTAR_PERSIANA_DORMITORIO:
+                consultarPersianaDormitorio();
+                break;
+            case SUBIR_PERSIANA_DORMITORIO:
+                subirPersianaDormitorio();
+                break;
+            case BAJAR_PERSIANA_DORMITORIO:
+                bajarPersianaDormitorio();
+                break;
+            case MEDIAALTURA_PERSIANA_DORMITORIO:
+                mediaAlturaPersianaDormitorio();
+                break;
+            case ENCENDER_LUCES_DORMITORIO:
+                encenderLucesDormitorio();
+                break;
+            case APAGAR_LUCES_MANUAL_DORMITORIO:
+                apagarLucesManualDormitorio();
+                break;
+            case APAGAR_LUCES_GENERAL_DORMITORIO:
+                apagarLucesGeneralDormitorio();
+                break;
+            case APAGAR_LUCES_ECO_DORMITORIO:
+                apagarLucesEcoDormitorio();
+                break;
+            case CONSULTAR_LUCES_DORMITORIO:
+                consultarLucesDormitorio();
+                break;
+            case ENCENDER_VIGILANCIA_DORMITORIO:
+                encenderVigilanciaDormitorio();
+                break;
+            case APAGAR_VIGILANCIA_DORMITORIO:
+                apagarVigilanciaDormitorio();
+                break;
+            case CONSULTAR_VIGILANCIA_DORMITORIO:
+                consultarVigilanciaDormitorio();
+                break;
+            case CONSULTAR_ESTADO_GENERAL:
+                consultarEstadoGeneral();
+                break;
         }
     }
 
@@ -69,5 +144,107 @@ public class Central {
     
     private boolean consultarEstadoGaraje(){
         return garaje.getPuertaGaraje().isEstado();
+    }
+    
+    private boolean consultarEstadoPersianaSalon(){
+        
+    }
+    
+    private void subirPersianaSalon(){
+        
+    }
+    
+    private void bajarPersianaSalon(){
+        
+    }
+    
+    private void mediaAlturaPersianaSalon(){
+        
+    }
+    
+    private void encenderLucesSalon(){
+        
+    }
+    
+    private void apagarLucesManualSalon(){
+        
+    }
+    
+    
+    private void apagarLucesGeneralSalon(){
+        
+    }
+    
+    private void apagarLucesEcoSalon(){
+        
+    }
+    
+    private boolean consultarLucesSalon(){
+        
+    }
+    
+    private void encenderVigilanciaSalon(){
+        
+    }
+    
+    private void apagarVigilanciaSalon(){
+        
+    }
+    
+    private boolean consultarVigilanciaSalon(){
+        
+    }
+    
+    private boolean consultarPersianaDormitorio(){
+        
+    }
+    
+    
+    private void subirPersianaDormitorio(){
+        
+    }
+    
+    private void bajarPersianaDormitorio(){
+        
+    }
+    
+    private void mediaAlturaPersianaDormitorio(){
+        
+    }
+    
+    private void encenderLucesDormitorio(){
+        
+    }
+    
+    private void apagarLucesManualDormitorio(){
+        
+    }
+    
+    private void apagarLucesGeneralDormitorio(){
+        
+    }
+    
+    private void apagarLucesEcoDormitorio(){
+        
+    }
+    
+    private boolean consultarLucesDormitorio(){
+        
+    }
+    
+    private void encenderVigilanciaDormitorio(){
+        
+    }
+    
+    private void apagarVigilanciaDormitorio(){
+        
+    }
+    
+    private boolean consultarVigilanciaDormitorio(){
+        
+    }
+    
+    private boolean consultarEstadoGeneral(){
+        
     }
 }
