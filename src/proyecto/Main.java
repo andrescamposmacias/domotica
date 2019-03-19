6b62d24113f69c9aaedf6aa378e76f20123c2435
Andres <andrescamposmacias@gmail.com>
2019-03-20 00:24:11 +0100

    modificar el objeto central con el nuevo constructor de persiana y usar el metodo ejecutarOrden

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -22,15 +22,15 @@ public class Main {
 //        
 //        Garaje pruebaGaraje = new Garaje(2, new Puerta(false, 3), 50);
 //        System.out.println(g2);
-
-
-        
-        Vista.menu();
         
         Central prueba = new Central(new Garaje(2, new Puerta(false, 3), 50),
-                new Salon (false, new Luz(true, 10.0), new Persiana(1, 30,50), new Camara(false, "192.168.1.10"), 20), 
-                new Dormitorio(Orientacion.NORTE, new Luz(true, 10.0), new Persiana(0, 20,30), new Camara(false, "192.168.1.11"), 30), 
+                new Salon (false, new Luz(true, 10.0), new Persiana(EstadoPersiana.BAJADA, 30,50), new Camara(false, "192.168.1.10"), 20), 
+                new Dormitorio(Orientacion.NORTE, new Luz(true, 10.0), new Persiana(EstadoPersiana.SUBIDA, 20,30), new Camara(false, "192.168.1.11"), 30), 
                 new Usuario("andres", "diego"), diaHoy, "primera central creada" );
+        
+        prueba.ejecutarOrden(Vista.menu());
+        
+        
                 
     }
     
