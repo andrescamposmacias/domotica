fd5642a63a4e851156ddd010473c07c4728a9990
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:32:29 +0100

    añadir el constructor, getters y setters y el metodo toString

--- src/proyecto/Camara.java
+++ src/proyecto/Camara.java
@@ -13,5 +13,31 @@ public class Camara {
     
     private boolean estado;
     private String ip; //direccion ip de la camara
+
+    public Camara(boolean estado, String ip) {
+        this.estado = estado;
+        this.ip = ip;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Override
+    public String toString() {
+        return "Camara{" + "estado=" + estado + ", ip=" + ip + '}';
+    }
     
 }
