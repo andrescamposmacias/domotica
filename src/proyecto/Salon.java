ed5d032de26105650e0cf92c5b5efcb803f9bf9c
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:37:18 +0100

    añadir el constructor, getters y setters y el metodo toString

--- src/proyecto/Salon.java
+++ src/proyecto/Salon.java
@@ -13,10 +13,22 @@ public class Salon extends Habitaciones {
     
     private boolean terraza;
 
-    public Salon(boolean terraza) {
+    public Salon(boolean terraza, Luz luz, Persiana persiana, Camara camara, int m2) {
+        super(luz, persiana, camara, m2);
         this.terraza = terraza;
     }
-    
-    
+
+    public boolean isTerraza() {
+        return terraza;
+    }
+
+    public void setTerraza(boolean terraza) {
+        this.terraza = terraza;
+    }
+
+    @Override
+    public String toString() {
+        return "Salon{" + "terraza=" + terraza + '}';
+    }
     
 }
