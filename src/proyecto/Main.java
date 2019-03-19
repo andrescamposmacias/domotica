257baac57d1d88ae1fddd3e8ed1e6aeb28db04c4
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:42:35 +0100

    modificacion del objeto central añadiendo los nuevos parametros de los constructores

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -28,7 +28,8 @@ public class Main {
         Vista.menu();
         
         Central prueba = new Central(new Garaje(2, new Puerta(false, 3), 50),
-                new Salon (false), new Dormitorio(Orientacion.NORTE), 
+                new Salon (false, new Luz(true, 10.0), new Persiana(1, 30,50), new Camara(false, "192.168.1.10"), 20), 
+                new Dormitorio(Orientacion.NORTE, new Luz(true, 10.0), new Persiana(0, 20,30), new Camara(false, "192.168.1.11"), 30), 
                 new Usuario("andres", "diego"), diaHoy, "primera central creada" );
                 
     }
