3ba9ca14237ca6aa3956271a79b53eff9c0faa44
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:36:01 +0100

    añadir el constructor, getters y setters y el metodo toString, y comprobar que en el constructor y en el setter no introduzca un estado que no exista

--- src/proyecto/Persiana.java
+++ src/proyecto/Persiana.java
@@ -11,8 +11,54 @@ package proyecto;
  */
 public class Persiana {
     
-    private boolean estado;
+    private int estado;
     private int largo;//cm
     private int ancho;//cm
+
+    public Persiana(int estado, int largo, int ancho) {
+        if(estado >= 0 || estado <= 2){
+            this.estado = estado;
+        }else{
+            this.estado = 0;
+        }
+        
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        if(estado >= 0 || estado <= 2){
+            this.estado = estado;
+        }else{
+            this.estado = 0;
+        }
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    @Override
+    public String toString() {
+        return "Persiana{" + "estado=" + estado + ", largo=" + largo + ", ancho=" + ancho + '}';
+    }
+    
+    
     
 }
