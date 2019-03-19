da01666033d3af6b739da833ab9be4b499857cc1
Andres <andrescamposmacias@gmail.com>
2019-03-19 23:34:19 +0100

    añadir el constructor, getters y setters y el metodo toString

--- src/proyecto/Habitaciones.java
+++ src/proyecto/Habitaciones.java
@@ -14,6 +14,41 @@ public class Habitaciones extends Estancia {
     private Luz luz;
     private Persiana persiana;
     private Camara camara;
-    
+
+    public Habitaciones(Luz luz, Persiana persiana, Camara camara, int m2) {
+        super(m2);
+        this.luz = luz;
+        this.persiana = persiana;
+        this.camara = camara;
+    }
+
+    public Luz getLuz() {
+        return luz;
+    }
+
+    public void setLuz(Luz luz) {
+        this.luz = luz;
+    }
+
+    public Persiana getPersiana() {
+        return persiana;
+    }
+
+    public void setPersiana(Persiana persiana) {
+        this.persiana = persiana;
+    }
+
+    public Camara getCamara() {
+        return camara;
+    }
+
+    public void setCamara(Camara camara) {
+        this.camara = camara;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitaciones{" + "luz=" + luz + ", persiana=" + persiana + ", camara=" + camara + '}';
+    }  
     
 }
