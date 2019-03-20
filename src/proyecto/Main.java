49d1788d0642b27709dcac57dd2ff9ebf82cb135
Andres <andrescamposmacias@gmail.com>
2019-03-20 12:46:11 +0100

    cambiar el main añadiendo la comprobacionUsuario

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -14,24 +14,33 @@ import java.time.LocalDate;
 public class Main {
     
     public static void main(String[] args) {
-        
-//        Garaje g1 = new Garaje();
-//        System.out.println(g1);
+ 
         LocalDate diaHoy = LocalDate.now();
-
-//        
-//        Garaje pruebaGaraje = new Garaje(2, new Puerta(false, 3), 50);
-//        System.out.println(g2);
         
         Central prueba = new Central(new Garaje(2, new Puerta(false, 3), 50),
                 new Salon (false, new Luz(true, 10.0), new Persiana(EstadoPersiana.BAJADA, 30,50), new Camara(false, "192.168.1.10"), 20), 
                 new Dormitorio(Orientacion.NORTE, new Luz(true, 10.0), new Persiana(EstadoPersiana.SUBIDA, 20,30), new Camara(false, "192.168.1.11"), 30), 
                 new Usuario("andres", "diego"), diaHoy, "primera central creada" );
         
-        prueba.ejecutarOrden(Vista.menu());
-        
-        
+                int contador = 0;
+                boolean continuar = true;
                 
+                do{
+                    if (prueba.comprobacionUsuario()) {
+                        prueba.ejecutarOrden(Vista.menu());
+                        continuar = false;
+                    } else {
+                        System.out.println("Usuario y/o contraseña incorrecta");
+                        contador++;
+                    }
+                    
+                }while(continuar && contador != 5);
+                
+           
+        
+        
+            
+                   
     }
     
 }
