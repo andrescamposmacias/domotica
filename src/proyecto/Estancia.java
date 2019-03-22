4a61a9f0052f9fa9dbab803b4c1462e158eea251
Andres <andrescamposmacias@gmail.com>
2019-03-22 10:57:19 +0100

    añadir comentarios

--- src/proyecto/Estancia.java
+++ src/proyecto/Estancia.java
@@ -10,17 +10,17 @@ package proyecto;
  * @author andres
  */
 public class Estancia {
-    
+    //creo el atributo
     private int m2;
-    
+    //el constructor por defecto
     public Estancia(){
         m2 = 0;
     }
-    
+    //constructor parametrizado
     public Estancia(int m2){
         this.m2 = m2;
     }
-
+    //getters y setters
     public int getM2() {
         return m2;
     }
@@ -28,7 +28,7 @@ public class Estancia {
     public void setM2(int m2) {
         this.m2 = m2;
     }
-
+    //el toString
     @Override
     public String toString() {
         return "Estancia{" + "m2=" + m2 + '}';
