bc1cbfb1cb31557869dc373c0b2b378740df45ab
Andres <andrescamposmacias@gmail.com>
2019-03-22 10:56:47 +0100

    añadir comentarios

--- src/proyecto/Dormitorio.java
+++ src/proyecto/Dormitorio.java
@@ -10,14 +10,17 @@ package proyecto;
  * @author andres
  */
 public class Dormitorio extends Habitaciones{
-    
+    //creo un atributo de orientacion que es un enum
+    //para asegurarme que la orientacion solo pueda ser
+    //norte, sur, este o oeste
     private Orientacion orientacion;
 
+    //constructor
     public Dormitorio(Orientacion orientacion, Luz luz, Persiana persiana, Camara camara, int m2) {
         super(luz, persiana, camara, m2);
         this.orientacion = orientacion;
     }
-
+    //getter y setters
     public Orientacion getOrientacion() {
         return orientacion;
     }
@@ -25,7 +28,8 @@ public class Dormitorio extends Habitaciones{
     public void setOrientacion(Orientacion orientacion) {
         this.orientacion = orientacion;
     }
-
+    
+    //el toString
     @Override
     public String toString() {
         return "Dormitorio{" + "orientacion=" + orientacion + '}';
