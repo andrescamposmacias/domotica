b6cfae80c521ba9e7d41ed84e393fc25c1a8858d
Andres <andrescamposmacias@gmail.com>
2019-03-22 10:59:05 +0100

    añadir comentarios

--- src/proyecto/Salon.java
+++ src/proyecto/Salon.java
@@ -10,14 +10,14 @@ package proyecto;
  * @author andres
  */
 public class Salon extends Habitaciones {
-    
+    //el atributo
     private boolean terraza;
-
+    //el constructor
     public Salon(boolean terraza, Luz luz, Persiana persiana, Camara camara, int m2) {
         super(luz, persiana, camara, m2);
         this.terraza = terraza;
     }
-
+    //getters y setters
     public boolean isTerraza() {
         return terraza;
     }
@@ -25,7 +25,7 @@ public class Salon extends Habitaciones {
     public void setTerraza(boolean terraza) {
         this.terraza = terraza;
     }
-
+    //el toString
     @Override
     public String toString() {
         return "Salon{" + "terraza=" + terraza + '}';
